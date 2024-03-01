866e10bbb253486f0863f9e6d707e32be122e87c
Fvelizb <155122487+Fvelizb@users.noreply.github.com>
2024-03-01 03:01:37 +0000

    "generador de excusas"

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let action = ['ate', 'peed', 'crushed', 'broke'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let what = ['my homework', 'the keys', 'the car'];
+
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+function generateExcuse() {
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    let excuse = who[whoIndex] + ' ' + action[actionIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex];
+    return excuse;
+}
+
+document.getElementById('generate-excuse').onclick = function() {
+    document.getElementById('excuse').innerText = generateExcuse();
 };
